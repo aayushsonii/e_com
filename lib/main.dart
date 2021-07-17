0bde1ffa7379e294727ba084e9d5cd9d0ba7cc0a
aayushsonii <Saraayushaf@gmail.com>
2021-07-17 22:09:14 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,22 @@
-import 'package:e_com/Views/landing_page.dart';
-import 'package:e_com/Views/login_page.dart';
-import 'package:e_com/Views/registration_page.dart';
-import 'package:e_com/constants.dart';
+import 'package:e_com/screens/landing_page.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  // Get.lazyPut(() => CustomButtonController());
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-      ),
-      home: Scaffold(
-        body: LandingPage(),
-      ),
-      getPages: [
-        GetPage(name: kLogInScreenRoute, page: () => LoginPage()),
-        GetPage(name: kRegistrationScreenRoute, page: () => RegistrationPage()),
-        GetPage(name: kLandingScreenRoute, page: () => LandingPage()),
-        // GetPage(name: "/fourth", page: () => Fourth()),
-      ],
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          accentColor: Color(0xFFFF1E00)),
+      home: LandingPage(),
     );
   }
 }
